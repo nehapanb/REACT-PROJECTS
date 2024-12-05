6946684e17423026f3115d854378b4a8d1130173
Neha panbude <nehapanbude123@gmail.com>
2024-12-05 12:12:36 +0530

    first commit

---
+++ first project/src/App.jsx
@@ -0,0 +1,23 @@
+import'./App.css'
+
+function App() {
+
+  let st={
+    backgroundColor:"white",
+    color:"brown"
+  }
+  return(
+    <>
+    <nav/>
+       <h1 style={{backgroundColor:"pink", color:"White"}}>welcome to react</h1>
+      <h2 style={st}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam vel cum nostrum rem aut? Veritatis aperiam repellendus deleniti omnis voluptatem, minus impedit molestias aut quidem debitis earum deserunt libero ducimus.</h2>
+      <h1 className="head1">create your ideas</h1>
+      <br/>
+    
+    </>
+
+  )
+}
+
+export default App
+
---
+++ first project/src/navbar.jsx
@@ -0,0 +1,15 @@
+const Nav =()=> {
+    return(
+        <>
+        <nav classname="Navbar">
+            <h1>Logo</h1>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contacts</li>
+                <li>Services</li>
+            </ul>
+        </nav>
+        </>
+        )
+        }
